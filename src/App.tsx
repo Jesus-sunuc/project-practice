f8eeb6e4f22fc4a4c5ccba0968cbe60948d526af
Jesus Sunuc <mariano.code4guate@gmail.com>
2025-05-16 13:11:01 -0600

    first api calls are shown in the browser

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+type Book = {
+  title: string;
+};
+
 function App() {
+  const API_URL = import.meta.env.VITE_API_URL;
+  const [books, setBooks] = useState<Book[]>();
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}books`)
+      .then((data) => {
+        console.log(data);
+        setBooks(data.data);
+      })
+      .catch(() => {
+        console.error("Failed");
+      });
+  }, []);
+
+  if (!books)
+    return (
+      <div className="d-flex justify-content-center">
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
 
   return (
     <>
-     <h1 className="text-danger">Hola Mundo</h1>
+      <div className="container">
+        <div className="row">
+          <h1 className="text-success mt-4">Harry Poter's Books</h1>
+          <div>
+            <ol>
+              {books.map((book, index) => (
+                <li key={index}>Name: {book.title}</li>
+              ))}
+            </ol>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
